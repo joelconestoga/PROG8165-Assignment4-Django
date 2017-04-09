87ff40a131d6883f606297f39798ae64b28ee6a6
joel matsu <matsu.joel@gmail.com>
2017-04-09 16:43:58 -0400

    implemented login and register - no validations yet

--- transaction_manager/manager/urls.py
+++ transaction_manager/manager/urls.py
@@ -5,4 +5,6 @@ app_name = 'manager'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^log_in/$', views.log_in, name='log_in'),
+	url(r'^register/$', views.register, name='register'),
 ]
--- transaction_manager/manager/views.py
+++ transaction_manager/manager/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import RegisterForm
 
 # Create your views here.
 def index(request) :
 	return render(request, 'manager/index.html')
+
+
+def log_in(request):
+	
+	if request.method != "POST":
+		return render(request, 'manager/log_in.html')
+		
+	username = request.POST['username']
+	password = request.POST['password']
+
+	user = authenticate(username=username, password=password)
+
+	if user is None:
+		return render(request, 'manager/log_in.html', {'error_message': 'Invalid login/password.'})
+
+	login(request, user)
+	
+	return index(request)
+
+
+def register(request):
+	form = RegisterForm(request.POST or None)
+
+	if form.is_valid():
+
+		user = form.save(commit=False)
+
+		username = form.cleaned_data['username']
+		password = form.cleaned_data['password']
+		user.set_password(password)
+
+		user.save()
+
+		user = authenticate(username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return index(request)
+
+	return render(request, 'manager/register.html', {'form': form})
