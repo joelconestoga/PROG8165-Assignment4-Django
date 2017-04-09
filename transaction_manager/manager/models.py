310ad2174fb73319a9ffdac810fdac98079b2469
Burak Karahan <bkarahan1062@conestogac.on.ca>
2017-04-09 18:46:43 -0400

    added a string method for transactions

--- transaction_manager/manager/models.py
+++ transaction_manager/manager/models.py
@@ -7,3 +7,6 @@ class Transaction(models.Model):
 	user = models.ForeignKey(User, default=1)
 	name = models.CharField(max_length=100)
 	value = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def __str__(self):
+		return self.name + ' - ' + str(self.value)
